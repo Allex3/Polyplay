3bed8e026a2cea108a9febdaeb05db431befac1f
Allex3 <103828759+Allex3@users.noreply.github.com>
2026-05-05 05:18:42 +0300

    Added WebSocket Chat Functionality (hope it works)

--- BackEnd/PolyplayAPI/Controllers/Chat/GeneralChatController.cs
+++ BackEnd/PolyplayAPI/Controllers/Chat/GeneralChatController.cs
@@ -1,14 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PolyplayAPI.Models;
 using PolyplayAPI.Models.Chats;
 using PolyplayAPI.Services;
+using System.Net.WebSockets;
+using System.Text;
 
 namespace PolyplayAPI.Controllers.Chat;
 
 [ApiController]
 [Route("api/[controller]")]
-public class GeneralChatController(GeneralChatService generalChatService) : ControllerBase
+public class GeneralChatController(GeneralChatService generalChatService, ConcurrentDictionary<string, WebSocket> wsConnections) : ControllerBase
 {
     private readonly GeneralChatService _generalChatService = generalChatService;
+    private readonly ConcurrentDictionary<string, WebSocket> _wsConnections = wsConnections;
+
+    [Route("~/ws/generalChat")] // ~ overrides default routing, so
+    // instead of api/games/ws/... it will be just /ws/startTestGames
+    public async Task EstablishGeneralChatWs()
+    {   // HttpContext of the executing action
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            WebSocket webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+            CancellationToken ct = HttpContext.RequestAborted;
+
+            if (ct.IsCancellationRequested)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest; // idk
+            }
+            string webSocketId = Guid.NewGuid().ToString(); // global ID for this websocket connection
+
+            wsConnections.TryAdd(webSocketId, webSocket); // to broadcast later
+
+            await StartGeneralChatForUser(webSocket, webSocketId);
+
+        }
+
+        else
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest; // not websocket request
+        }
+    }
+    private async Task StartGeneralChatForUser(WebSocket webSocket, string webSocketId)
+    {
+        const int THIS_IS_NOT_A_MAGIC_NUMBER = 1024;
+
+        CancellationToken ct = CancellationToken.None;
+
+        do
+        {
+
+            // this while just runs and waits to receive messages, then broadcasts them to the other clients
+
+            GeneralChatMessage receivedMessage = await WsUtilities.ReadJsonAsync<GeneralChatMessage>(webSocket, ct);
+            if (receivedMessage == null)
+            {
+                if (webSocket.State != WebSocketState.Open)
+                {
+                    break;
+                }
+
+                continue; // act as if message doesn't exist
+            }
+
+            await _generalChatService.CreateAsync(receivedMessage);
+
+            foreach (var connection in _wsConnections) // not working broadcast
+            {
+                if (connection.Value.State != WebSocketState.Open)
+                {
+                    continue;
+                }
+
+                await WsUtilities.SendJsonAsync<GeneralChatMessage>(connection.Value, receivedMessage, ct);
+            }
+        } while (!ct.IsCancellationRequested);
+
+        WebSocket dummy;
+
+        _wsConnections.TryRemove(webSocketId, out dummy);
+        await webSocket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "UserDisconnected", ct);
+
+        webSocket.Dispose();
+
+    }
 
     [HttpGet]
     public async Task<List<GeneralChatMessage>> Get() => await _generalChatService.GetAsync();
---
+++ BackEnd/PolyplayAPI/Controllers/Chat/WsUtilities.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.WebSockets;
+using System.Text.Json;
+using PolyplayAPI.Models.Chats;
+
+namespace PolyplayAPI.Controllers.Chat
+{
+    public static class WsUtilities
+    {
+        private const int THIS_IS_NOT_A_MAGIC_NUMBER = 1024;
+        public static async Task<T> ReadJsonAsync<T>(WebSocket ws, CancellationToken ct = default)
+        {
+            var buffer = new ArraySegment<byte>(new byte[THIS_IS_NOT_A_MAGIC_NUMBER*16]); //16kb
+
+            using MemoryStream ms = new MemoryStream();
+            WebSocketReceiveResult receivedResult;
+
+            do
+            {
+                ct.ThrowIfCancellationRequested();
+
+                receivedResult = await ws.ReceiveAsync(buffer, ct);
+
+                ms.Write(buffer.Array, buffer.Offset, receivedResult.Count);
+
+            } while (!receivedResult.EndOfMessage);
+
+
+            ms.Seek(0, SeekOrigin.Begin); // Changing stream position to cover whole message
+
+
+            if (receivedResult.MessageType != WebSocketMessageType.Text)
+                return default(T);
+
+            using var reader = new StreamReader(ms, System.Text.Encoding.UTF8);
+            return JsonSerializer.Deserialize <T>(await reader.ReadToEndAsync(ct)); // decoding message
+        }
+
+        public static Task SendJsonAsync<T>(WebSocket ws, T data, CancellationToken ct = default)
+        {
+            var buffer = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(data));
+            var segment = new ArraySegment<byte>(buffer);
+            return ws.SendAsync(segment, WebSocketMessageType.Text, true, ct);
+        }
+    }
+}
